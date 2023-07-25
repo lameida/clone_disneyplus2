7c479328d562fa0a174e6b4b5c773715ffe98a15
Idaiane Cavalcante <lalmeida0811@gmail.com>
2023-07-25 20:00:51 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-questions]');
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const posicaoAtual =  window.scrollY;
+
+        if (posicaoAtual < alturaHero ){
+            ocultaElementosDoHeader();
+        } else {
+            exibeElementosDoHeader();
+        }
+    })
     
 
     for (let i = 0; i < buttons.length; i++) {
@@ -13,7 +27,28 @@ document.addEventListener('DOMContentLoaded', function() {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    for (let i = 0; i< questions.length; i++) {
+        questions[i].addEventListener('click' , abreOuFechaResposta);
+    }
 })
+
+function ocultaElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosDoHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function abreOuFechaResposta(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoativo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
@@ -28,7 +63,7 @@ function escondeTodasAbas() {
     const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
 
-    for (let i = 0; i < tabsContainer.lenght; i++ ) {
+    for (let i = 0; i < tabsContainer.length; i++ ) {
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
 }
